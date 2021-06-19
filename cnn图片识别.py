146f2a88627d2902e829293a682914a66a1ae7de
troye-wang <1984611527@qq.com>
2021-06-19 22:29:41 +0800

    cifar

---
+++ cnn图片识别.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+#数据集归一化(cifar用ANN训练效率低改用CNN）
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+#搭建模型三层2D卷积层
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+#添加分类器
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#训练模型，评估
+model.fit(x_train, y_train, epochs=5,
+          validation_data=(x_test, y_test))
+
+
+
+
